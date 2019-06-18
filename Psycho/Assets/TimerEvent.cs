614d2dcc608d949e86bf6caf50680e522d724023
Michael <mejevin@gmail.com>
2019-06-18 11:14:36 +0300

    Black overlay for initialization

---
+++ Psycho/Assets/TimerEvent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TimerEvent : MonoBehaviour
+{
+    public UnityEvent OnTimerDone;
+
+    public float TimeTotal;
+
+    private bool IsGoing;
+    private float TimeLeft;
+
+    void Awake()
+    {
+        IsGoing = false;
+    }
+
+    public void StartTimer()
+    {
+        TimeLeft = TimeTotal;
+        IsGoing = true;
+    }
+    public void StopTimer()
+    {
+        IsGoing = false;
+    }
+
+    void Update()
+    {
+        if (IsGoing)
+        {
+            TimeLeft -= Time.deltaTime;
+
+            if (TimeLeft <= 0)
+            {
+                StopTimer();
+
+                OnTimerDone.Invoke();
+            }
+        }
+    }
+}
