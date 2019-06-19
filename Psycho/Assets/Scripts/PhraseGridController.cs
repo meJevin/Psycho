a842fe6fba209f78df9fa41164327b6583387f74
Michael <mejevin@gmail.com>
2019-06-20 00:28:32 +0300

    Added grid for phrases

---
+++ Psycho/Assets/Scripts/PhraseGridController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhraseGridController : MonoBehaviour
+{
+    public GameObject Dummy;
+
+    public GameObject Content;
+
+    public int amount = 10;
+
+    void Start()
+    {
+        for (int i = 0; i < amount; ++i)
+        {
+            AddPhraseItem();
+        }
+    }
+
+    public void AddPhraseItem()
+    {
+        GameObject objAdded = GameObject.Instantiate(Dummy, Content.transform);
+    }
+}
